e9ef950a157ff2606ebc02260c9bde242158bda5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-13 08:10:28 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -67,13 +67,13 @@ void main() async {
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
       await tester.tap(find.text('Cancel'));
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
+      await tester.enterText(find.byKey(ValueKey('btnSearch')), 'Hello World');
+      await tester.pumpAndSettle(Duration(milliseconds: 5000));
       await tester.tap(find.byKey(ValueKey('btnOption')));
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
       await tester.tap(find.text('View'));
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
       await tester.tap(find.text('Cancel'));
-      await tester.pumpAndSettle(Duration(milliseconds: 5000));
-      await tester.enterText(find.byKey(ValueKey('btnSearch')), 'Hello World');
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
       await tester.tap(find.byKey(ValueKey('btnOption')));
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
@@ -84,6 +84,12 @@ void main() async {
       await tester.tap(find.text('Jan'));
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
       await tester.tap(find.byKey(ValueKey('txtDescription')));
+      await tester.pumpAndSettle(Duration(milliseconds: 5000));
+      await tester.tap(find.text('Save'));
+      await tester.pumpAndSettle(Duration(milliseconds: 5000));
+      await tester.tap(find.text('btnOption'));
+      await tester.pumpAndSettle(Duration(milliseconds: 5000));
+      await tester.tap(find.text('Delete'));
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
       await tester.tap(find.text('Save'));
       await tester.pumpAndSettle(Duration(milliseconds: 5000));
